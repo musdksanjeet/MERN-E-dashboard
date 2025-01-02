02f8cf5dc6c7ceb1dd9fb965d2daabea4c6d6682
sanjeet <sanjeetsingh7877@gmail.com>
2025-01-02 18:24:10 +0530

    added

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React,{useState} from "react";
 
 const SignUp = ()=>{
+    const [name,setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    const registerData =()=>{
+        console.log(name,email,password);
+    }
     return(
-        <div className="signup-page">
+        <div className="register-page">
             <h3>Register</h3>
-            <input type="text" placeholder="Enter Name"/>
-            <input type="text" placeholder="Enter Email"/>
-            <input type="password" placeholder="Enter Password"/>
-            <button className="register-btn">Register</button>
+            <input className="register-input" type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder="Enter Name"/>
+            <input className="register-input" type="text" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Enter Email"/>
+            <input className="register-input" type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Enter Password"/>
+            <button className="register-btn" onClick={registerData}>Register</button>
         </div>
     );
 }
